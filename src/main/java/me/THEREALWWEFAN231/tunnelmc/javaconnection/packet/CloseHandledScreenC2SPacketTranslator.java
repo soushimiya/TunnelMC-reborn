89f8eded1927a41ba04bb4c34dd74ac88f1e637e
Flonja <20887403+Flonja@users.noreply.github.com>
2022-11-30 09:40:40 +0100

    Delete unneeded mixin

--- src/main/java/me/THEREALWWEFAN231/tunnelmc/javaconnection/packet/CloseHandledScreenC2SPacketTranslator.java
+++ src/main/java/me/THEREALWWEFAN231/tunnelmc/javaconnection/packet/CloseHandledScreenC2SPacketTranslator.java
@@ -3,7 +3,6 @@ package me.THEREALWWEFAN231.tunnelmc.javaconnection.packet;
 import com.nukkitx.protocol.bedrock.packet.ContainerClosePacket;
 
 import me.THEREALWWEFAN231.tunnelmc.bedrockconnection.Client;
-import me.THEREALWWEFAN231.tunnelmc.mixins.interfaces.IMixinCloseHandledScreenC2SPacket;
 import me.THEREALWWEFAN231.tunnelmc.translator.PacketTranslator;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 
@@ -11,7 +10,7 @@ public class CloseHandledScreenC2SPacketTranslator extends PacketTranslator<Clos
 
 	@Override
 	public void translate(CloseHandledScreenC2SPacket packet) {
-		byte id = (byte) ((IMixinCloseHandledScreenC2SPacket) packet).getSyncId();
+		byte id = (byte) packet.getSyncId();
 		if (id == 0) {
 			// The main inventory being closed does not send a container close packet.
 			// Sending this on PocketMine servers also crashes the client.
