fea07e1028efcfe4e0662e9d68dcfde927bb86c8
Flonja <20887403+Flonja@users.noreply.github.com>
2022-12-07 23:35:53 +0100

    Redo packet sending logic and some small refactoring

--- src/main/java/me/THEREALWWEFAN231/tunnelmc/javaconnection/packet/movement/PlayerMoveTranslator.java
+++ src/main/java/me/THEREALWWEFAN231/tunnelmc/javaconnection/packet/movement/PlayerMoveTranslator.java
@@ -1,7 +1,9 @@
 package me.THEREALWWEFAN231.tunnelmc.javaconnection.packet.movement;
 
+import com.darkmagician6.eventapi.EventManager;
 import com.darkmagician6.eventapi.EventTarget;
 import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.protocol.bedrock.data.ClientPlayMode;
 import com.nukkitx.protocol.bedrock.data.InputInteractionModel;
 import com.nukkitx.protocol.bedrock.data.InputMode;
 import com.nukkitx.protocol.bedrock.data.PlayerAuthInputData;
@@ -15,13 +17,14 @@ import net.minecraft.entity.EntityPose;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class PlayerMoveTranslator extends PacketTranslator<PlayerMoveC2SPacket> {
-	private static double lastPosX;
-	private static double lastPosY = -Double.MAX_VALUE;
-	private static double lastPosZ;
-	private static float lastYaw;
-	private static float lastPitch;
+	private static Vector3f lastPosition = Vector3f.ZERO;
+	private static Vector3f lastRotation = Vector3f.ZERO; // x for pitch, y for yaw, z for head yaw
 	private static boolean lastOnGround;
 	private static long currentTick;
+
+	public PlayerMoveTranslator() {
+		EventManager.register(this);
+	}
 
 	@Override
 	public void translate(PlayerMoveC2SPacket packet) {
@@ -38,66 +41,74 @@ public class PlayerMoveTranslator extends PacketTranslator<PlayerMoveC2SPacket> 
 		double currentPosX = playerMoveC2SPacket.getX(TunnelMC.mc.player.getPos().x);
 		double currentPosY = playerMoveC2SPacket.getY(TunnelMC.mc.player.getPos().y) + TunnelMC.mc.player.getEyeHeight(EntityPose.STANDING);
 		double currentPosZ = playerMoveC2SPacket.getZ(TunnelMC.mc.player.getPos().z);
-		float currentYaw = playerMoveC2SPacket.getYaw(TunnelMC.mc.player.getYaw());
 		float currentPitch = playerMoveC2SPacket.getPitch(TunnelMC.mc.player.getPitch());
+		float currentYaw = playerMoveC2SPacket.getYaw(TunnelMC.mc.player.getYaw());
+		float currentHeadYaw = TunnelMC.mc.player.getHeadYaw();
 		boolean currentlyOnGround = playerMoveC2SPacket.isOnGround();
-
-		if (PlayerMoveTranslator.lastPosX == currentPosX && PlayerMoveTranslator.lastPosY == currentPosY
-				&& PlayerMoveTranslator.lastPosZ == currentPosZ && PlayerMoveTranslator.lastYaw == currentYaw
-				&& PlayerMoveTranslator.lastPitch == currentPitch && PlayerMoveTranslator.lastOnGround == currentlyOnGround) {
-			return;
-		}
 
 		int runtimeId = TunnelMC.mc.player.getId();
 		Vector3f currentPos = Vector3f.from(currentPosX, currentPosY, currentPosZ);
+		Vector3f currentRot = Vector3f.from(currentPitch, currentYaw, currentHeadYaw);
 
 		switch (Client.instance.movementMode) {
 			case CLIENT -> {
+				if (lastPosition.equals(currentPos) && lastRotation.equals(currentRot) && lastOnGround == currentlyOnGround) {
+					return;
+				}
+
 				MovePlayerPacket movePacket = new MovePlayerPacket();
 				movePacket.setRuntimeEntityId(runtimeId);
 				movePacket.setPosition(currentPos);
-				movePacket.setRotation(Vector3f.from(currentPitch, currentYaw, currentYaw)); // Set yaw twice so BDS cooperates with head movement better
+				movePacket.setRotation(Vector3f.from(currentPitch, currentYaw, currentHeadYaw));
 				movePacket.setMode(mode);
 				movePacket.setOnGround(currentlyOnGround);
 				Client.instance.sendPacket(movePacket);
 			}
 			case SERVER -> {
-				Vector3f previousPos = Vector3f.from(lastPosX, lastPosY, lastPosZ);
-
 				PlayerAuthInputPacket movePacket = new PlayerAuthInputPacket();
 				movePacket.setInputMode(InputMode.MOUSE);
+				movePacket.setPlayMode(ClientPlayMode.NORMAL);
 				movePacket.setInputInteractionModel(InputInteractionModel.CROSSHAIR);
-				movePacket.setDelta(currentPos.sub(previousPos));
+				movePacket.setVrGazeDirection(Vector3f.ZERO);
+				movePacket.setDelta(currentPos.sub(lastPosition));
+				movePacket.setMotion(movePacket.getDelta().toVector2(true));
 				movePacket.setPosition(currentPos);
 				movePacket.setRotation(Vector3f.from(currentPitch, currentYaw, currentYaw)); // Set yaw twice so BDS cooperates with head movement better
-				movePacket.setTick(currentTick);
 
-				if(Client.instance.startedSprinting.compareAndSet(true, false)) {
-					movePacket.getInputData().add(PlayerAuthInputData.START_SPRINTING);
-				}
-				if(Client.instance.stoppedSprinting.compareAndSet(true, false)) {
-					movePacket.getInputData().add(PlayerAuthInputData.STOP_SPRINTING);
-				}
-				if(Client.instance.startedSneaking.compareAndSet(true, false)) {
-					movePacket.getInputData().add(PlayerAuthInputData.START_SNEAKING);
-				}
-				if(Client.instance.stoppedSneaking.compareAndSet(true, false)) {
-					movePacket.getInputData().add(PlayerAuthInputData.STOP_SNEAKING);
-				}
-				Client.instance.sendPacket(movePacket);
+				playerAuthInputPacket = movePacket;
 			}
 		}
 
-		PlayerMoveTranslator.lastPosX = currentPosX;
-		PlayerMoveTranslator.lastPosY = currentPosY;
-		PlayerMoveTranslator.lastPosZ = currentPosZ;
-		PlayerMoveTranslator.lastYaw = currentYaw;
-		PlayerMoveTranslator.lastPitch = currentPitch;
+		PlayerMoveTranslator.lastPosition = currentPos;
+		PlayerMoveTranslator.lastRotation = currentRot;
 		PlayerMoveTranslator.lastOnGround = currentlyOnGround;
 	}
+
+	// For server authoritative movement, a vanilla client sends this packet every tick
+	private static PlayerAuthInputPacket playerAuthInputPacket;
 
 	@EventTarget
 	public void onEvent(EventPlayerTick event) {
 		currentTick++;
+
+		if(playerAuthInputPacket == null) {
+			return;
+		}
+
+		playerAuthInputPacket.setTick(currentTick);
+		playerAuthInputPacket.getInputData().clear();
+		if(Client.instance.startedSprinting.compareAndSet(true, false)) {
+			playerAuthInputPacket.getInputData().add(PlayerAuthInputData.START_SPRINTING);
+		}
+		if(Client.instance.stoppedSprinting.compareAndSet(true, false)) {
+			playerAuthInputPacket.getInputData().add(PlayerAuthInputData.STOP_SPRINTING);
+		}
+		if(Client.instance.startedSneaking.compareAndSet(true, false)) {
+			playerAuthInputPacket.getInputData().add(PlayerAuthInputData.START_SNEAKING);
+		}
+		if(Client.instance.stoppedSneaking.compareAndSet(true, false)) {
+			playerAuthInputPacket.getInputData().add(PlayerAuthInputData.STOP_SNEAKING);
+		}
+		Client.instance.sendPacket(playerAuthInputPacket);
 	}
 }
