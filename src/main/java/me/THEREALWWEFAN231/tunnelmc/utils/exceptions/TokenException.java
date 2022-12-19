1cb786105409066a94e21e9982f7ac2e43b625fd
Flonja <20887403+Flonja@users.noreply.github.com>
2022-12-19 17:04:24 +0100

    Give TokenException a message

--- src/main/java/me/THEREALWWEFAN231/tunnelmc/utils/exceptions/TokenException.java
+++ src/main/java/me/THEREALWWEFAN231/tunnelmc/utils/exceptions/TokenException.java
@@ -1,4 +1,8 @@
 package me.THEREALWWEFAN231.tunnelmc.utils.exceptions;
 
 public class TokenException extends RuntimeException{
+
+    public TokenException() {
+        super("Your token may be expired, please log in again.");
+    }
 }
