a1823d145295e9b8f923ea95e0213d7a1a92ee19
Flonja <20887403+Flonja@users.noreply.github.com>
2023-01-01 19:59:19 +0100

    Remove useless fields

--- src/main/java/me/THEREALWWEFAN231/tunnelmc/translator/packet/PacketTranslator.java
+++ src/main/java/me/THEREALWWEFAN231/tunnelmc/translator/packet/PacketTranslator.java
@@ -5,8 +5,6 @@ import me.THEREALWWEFAN231.tunnelmc.connection.java.FakeJavaConnection;
 import me.THEREALWWEFAN231.tunnelmc.translator.VoidTranslator;
 
 public abstract class PacketTranslator<T> implements VoidTranslator<T> {
-	protected BedrockConnection bedrockConnection;
-	protected FakeJavaConnection javaConnection;
 
 	public boolean idleUntil(T packet, BedrockConnection bedrockConnection, FakeJavaConnection javaConnection) {
 		return true;
